37320c0a0a5d53e9f76ec4db8513b6f565b1a782
Andy Fragen <andy@thefragens.com>
2015-03-10 16:33:12 -0700

    double slash namespaces

--- drmc-medical-staff-governance.php
+++ drmc-medical-staff-governance.php
@@ -3,7 +3,7 @@
 Plugin Name:       DRMC Medical Staff Governance
 Plugin URI:        https://github.com/afragen/drmc-medical-staff-governance
 Description:       This plugin adds registration, custom user meta and other things to the DRMC Medical Staff website for web-based governance.
-Version:           1.9.13
+Version:           1.9.14
 Author:            Andy Fragen
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
